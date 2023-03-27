5067e82f1d77288c8a845835f441ad50c4750bb2
DESKTOP-CHOLDAU\Touzani <devyassinepro@gmail.com>
2023-03-27 07:21:09 +0200

    add hotjar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,17 @@ fpr("click");
 
   gtag('config', 'AW-975957367');
 </script>
+<!-- Hotjar Tracking Code for my site -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3424765,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
     </head>
     <body class="bg-light">
         <nav class="bg-white navbar navbar-expand-md navbar-light border-bottom sticky-top">
