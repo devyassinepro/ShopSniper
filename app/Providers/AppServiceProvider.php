0c8218eb9f9d9e31dc4a33c0f0df4dbba7a644d3
DESKTOP-CHOLDAU\Touzani <devyassinepro@gmail.com>
2023-06-22 21:34:01 +0200

    secure

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        // URL::forceScheme('https');
+        URL::forceScheme('https');
 
     }
 }
