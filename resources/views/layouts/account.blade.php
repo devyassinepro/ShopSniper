a7913443c487c566808b3357051877a1f523edae
Touzani <touzani@MacBook-Pro-de-Touzani.local>
2024-02-22 13:01:17 +0100

    update 2

--- resources/views/layouts/account.blade.php
+++ resources/views/layouts/account.blade.php
@@ -21,7 +21,7 @@
   <link rel="stylesheet" href="{{ asset('assets/css/dashlite.css?ver=3.2.0') }}" type="text/css">
 
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 
 
 <!-- Scripts -->
