59e029994dd011dd518ae81563a193381fdc4e24
Touzani <touzani@mbp-de-touzani.home>
2024-03-10 10:07:28 +0100

    fix livewire 5

--- resources/views/livewire/account/research/listproducts.blade.php
+++ resources/views/livewire/account/research/listproducts.blade.php
@@ -1,3 +1,5 @@
 <div>
+
+    <h2>Show Me List Products</h2>
     
 </div>
--- resources/views/livewire/account/research/product-research.blade.php
+++ resources/views/livewire/account/research/product-research.blade.php
@@ -1,5 +1,4 @@
-<div class="nk-content ">
-    <div class="nk-content-inner">
-                 <livewire:account.research.listproducts lazy/>
-        </div>
-    </div>    
+<div>
+     <livewire:account.research.listproducts lazy/>
+</div>
+                
